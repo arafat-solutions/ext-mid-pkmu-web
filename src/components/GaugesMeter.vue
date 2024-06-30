1487e341e14a0c7ee52872b1c888751692e18b19
Pandu Warjaya <panduwarjaya@gmail.com>
2024-06-30 17:15:21 +0700

    adjustment speedometer

--- src/components/GaugesMeter.vue
+++ src/components/GaugesMeter.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-    <vue-speedometer
-      :startColor="startColor"
-      :endColor="endColor"
-      :segments="9"
-      :customSegmentLabels="customSegmentLabels"
-    />
+    <div :ref="`speedometerContainer${index}`" v-for="(speedometer, index) in speedometers">
+      <vue-speedometer
+        :segmentColors="segmentColors"
+        :segments="customSegmentLabels.length"
+        :value="speedometer.value"
+        :minValue="0"
+        :maxValue="customSegmentLabels.length"
+        :customSegmentLabels="customSegmentLabels"
+        :currentValueText="speedometer.label"
+        :width="250"
+        :height="250"
+        :ringWidth="30"
+        :paddingHorizontal="-20"
+        :paddingVertical="-20"
+        :needleHeightRatio="0.75"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import VueSpeedometer from "vue-speedometer/slim"
+import VueSpeedometer from "vue-speedometer"
 
 export default {
   name: 'GaugesMeter',
@@ -19,83 +30,135 @@ export default {
   },
   data() {
     return {
+      speedometers: [
+        {
+          label: 'W',
+          value: 1
+        },
+        {
+          label: 'V',
+          value: 1
+        },
+        {
+          label: 'X',
+          value: 1
+        },
+        {
+          label: 'Y',
+          value: 1
+        },
+        {
+          label: 'Z',
+          value: 1
+        },
+        {
+          label: 'A',
+          value: 1
+        },
+      ],
       startColor: '#33CC33',
       endColor: '#FF471A',
       segmentColors: [
-        '#FFFFFF',
-        '#FFFFFF',
-        '#FFFFFF',
-        '#FFFFFF',
-        '#FFFFFF',
-        '#FEFF29',
-        '#FEFF29',
+        '#33CC33',
+        '#50D230',
+        '#71D82D',
+        '#96DE2A',
+        '#BEE427',
+        '#EAE924',
+        '#EFC521',
         '#F91A23',
         '#F91A23',
       ],
       customSegmentLabels: [
         {
-          text: "1",
-          position: "OUTSIDE",
+          text: 1,
+          position: "INSIDE",
           fontSize: "14px",
           color: "#000000",
         },
         {
-          text: "2",
-          position: "OUTSIDE",
+          text: 2,
+          position: "INSIDE",
           fontSize: "14px",
           color: "#000000",
         },
         {
-          text: "3",
-          position: "OUTSIDE",
+          text: 3,
+          position: "INSIDE",
           fontSize: "14px",
           color: "#000000",
         },
         {
-          text: "4",
-          position: "OUTSIDE",
+          text: 4,
+          position: "INSIDE",
           fontSize: "14px",
           color: "#000000",
         },
         {
-          text: "5",
-          position: "OUTSIDE",
+          text: 5,
+          position: "INSIDE",
           fontSize: "14px",
           color: "#000000",
         },
         {
-          text: "6",
-          position: "OUTSIDE",
+          text: 6,
+          position: "INSIDE",
           fontSize: "14px",
           color: "#000000",
         },
         {
-          text: "7",
-          position: "OUTSIDE",
+          text: 7,
+          position: "INSIDE",
           fontSize: "14px",
           color: "#000000",
         },
         {
-          text: "8",
-          position: "OUTSIDE",
+          text: 8,
+          position: "INSIDE",
           fontSize: "14px",
           color: "#000000",
         },
         {
-          text: "9",
-          position: "OUTSIDE",
+          text: 9,
+          position: "INSIDE",
           fontSize: "14px",
           color: "#000000",
         },
       ]
     };
   },
+  mounted() {
+    this.modifySVG();
+  },
+  methods: {
+    // For change position label W,V,X,Y,Z,A
+    modifySVG() {
+      // Find the target 'g' element and modify it
+      this.speedometers.forEach((speedometer, index) => {
+        const refName = `speedometerContainer${index}`;
+        const container = this.$refs[refName][0];  // Access the ref DOM element
+        if (container) {
+          const svgElement = container.querySelector('svg');
+          if (svgElement) {
+            const gElements = svgElement.getElementsByTagName('g');
+            if (gElements.length > 2) {
+              gElements[2].setAttribute('transform', 'translate(105, 110)');
+            }
+          }
+        }
+      });
+    },
+  },
 };
 </script>
 
-<style scoped>
-.speedo-segment:nth-child(9), /* 8th index in 0-based */
-.speedo-segment:nth-child(10) /* 9th index in 0-based */ {
-  fill: rgb(249, 26, 35) !important;
+<style>
+.speedometer {
+  border: 2px solid black;
+  border-radius: 50%;
+}
+
+.current-value {
+  margin-top: 2rem;
 }
 </style>
