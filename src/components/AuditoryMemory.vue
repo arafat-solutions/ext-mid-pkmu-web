82232814acdec543e1b76ffdbf9d6a29cfe37ba5
Pandu Warjaya <panduwarjaya@gmail.com>
2024-07-07 07:01:36 +0700

    add config

---
+++ src/components/AuditoryMemory.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-view">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numberOfTask: 3, //positive number
+      stringSize: 'ABC-DE-FG', //AB-CD-E, AB-CD-EF, ABC-DE-FG, ABC-DEF-GH, ABC-DEF-GHJ
+      includeDigits: true, //true or false
+      excludeVowels: true, //true or false
+      readingSpeed: 3, // 1-5
+    }
+  }
+}
+</script>
