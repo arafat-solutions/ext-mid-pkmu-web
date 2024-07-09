d8340dfbdeececf9290bc5ad8b950f426e31ab7d
Pandu Warjaya <panduwarjaya@gmail.com>
2024-07-09 14:21:50 +0700

    change checkbox

--- src/components/AccousticMemory.vue
+++ src/components/AccousticMemory.vue
@@ -4,7 +4,16 @@
       <div v-for="row in 10" :key="row" class="checkbox-row">
         <div v-for="col in (choicesLength * stringSizeLength)" :key="col" class="checkbox-item">
           <label :for="`checkbox-${row}-${col}`" v-if="(col % 3) === 1" class="mr-2">{{ String.fromCharCode(96 + Math.ceil(col / 3)) }})</label>
-          <input type="checkbox" :id="`checkbox-${row}-${col}`" />
+          <div class="checkbox-wrapper">
+            <label class="checkbox">
+              <input class="checkbox__trigger visuallyhidden" type="checkbox" :id="`checkbox-${row}-${col}`" :disabled="row !== currentTask" />
+              <span class="checkbox__symbol">
+                <svg aria-hidden="true" class="icon-checkbox" width="28px" height="28px" viewBox="0 0 28 28" version="1" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M4 14l8 7L24 7"></path>
+                </svg>
+              </span>
+            </label>
+          </div>
         </div>
       </div>
     </div>
@@ -15,6 +24,7 @@
 export default {
   data() {
     return {
+      currentTask: 1,
       numberOfTask: 3, //positive number
       stringSize: 'ABC-DE-FG', //AB-CD-E, AB-CD-EF, ABC-DE-FG, ABC-DEF-GH, ABC-DEF-GHJ
       includeDigits: true, //true or false
@@ -213,10 +223,125 @@ export default {
   }
 
   .checkbox-item label {
-    margin-left: 30px;
+    margin-left: 15px;
   }
 
   .mr-2 {
     margin-right: 1rem;
+  }
+
+  .checkbox-wrapper {
+    --s-xsmall: 0.625em;
+    --s-small: 1.2em;
+    --border-width: 1px;
+    --c-primary: #5F11E8;
+    --c-primary-20-percent-opacity: rgb(95 17 232 / 20%);
+    --c-primary-10-percent-opacity: rgb(95 17 232 / 10%);
+    --t-base: 0.4s;
+    --t-fast: 0.2s;
+    --e-in: ease-in;
+    --e-out: cubic-bezier(.11,.29,.18,.98);
+  }
+
+  .checkbox-wrapper .visuallyhidden {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
+
+  .checkbox-wrapper .checkbox {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  .checkbox-wrapper .checkbox + .checkbox {
+    margin-top: var(--s-small);
+  }
+  .checkbox-wrapper .checkbox__symbol {
+    display: inline-block;
+    display: flex;
+    border: var(--border-width) solid var(--c-primary);
+    position: relative;
+    border-radius: 0.1em;
+    width: 1.5em;
+    height: 1.5em;
+    transition: box-shadow var(--t-base) var(--e-out), background-color var(--t-base);
+    box-shadow: 0 0 0 0 var(--c-primary-10-percent-opacity);
+  }
+  .checkbox-wrapper .checkbox__symbol:after {
+    content: "";
+    position: absolute;
+    top: 0.5em;
+    left: 0.5em;
+    width: 0.25em;
+    height: 0.25em;
+    background-color: var(--c-primary-20-percent-opacity);
+    opacity: 0;
+    border-radius: 3em;
+    transform: scale(1);
+    transform-origin: 50% 50%;
+  }
+
+  .checkbox-wrapper .checkbox .icon-checkbox {
+    width: 1em;
+    height: 1em;
+    margin: auto;
+    fill: none;
+    stroke-width: 3;
+    stroke: currentColor;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-miterlimit: 10;
+    color: var(--c-primary);
+    display: inline-block;
+  }
+
+  .checkbox-wrapper .checkbox .icon-checkbox path {
+    transition: stroke-dashoffset var(--t-fast) var(--e-in);
+    stroke-dasharray: 30px, 31px;
+    stroke-dashoffset: 31px;
+  }
+
+  .checkbox-wrapper .checkbox__textwrapper {
+    margin: 0;
+  }
+
+  .checkbox-wrapper .checkbox__trigger:checked + .checkbox__symbol:after {
+    -webkit-animation: ripple 1.5s var(--e-out);
+            animation: ripple 1.5s var(--e-out);
+  }
+  .checkbox-wrapper .checkbox__trigger:checked + .checkbox__symbol .icon-checkbox path {
+    transition: stroke-dashoffset var(--t-base) var(--e-out);
+    stroke-dashoffset: 0px;
+  }
+  .checkbox-wrapper .checkbox__trigger:focus + .checkbox__symbol {
+    box-shadow: 0 0 0 0.25em var(--c-primary-20-percent-opacity);
+  }
+
+  @-webkit-keyframes ripple {
+    from {
+      transform: scale(0);
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+      transform: scale(20);
+    }
+  }
+
+  @keyframes ripple {
+    from {
+      transform: scale(0);
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+      transform: scale(20);
+    }
   }
 </style>
