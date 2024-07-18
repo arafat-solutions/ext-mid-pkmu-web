0ef29fe5d32d08a0005471b33464f7b70284b37e
Pandu Warjaya <panduwarjaya@gmail.com>
2024-07-18 14:55:04 +0700

    add button

--- src/components/InstrumentCoordination.vue
+++ src/components/InstrumentCoordination.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="app">
+  <div id="main-view">
     <div class="indicators">
-      <Attitude class="indicator-bg" :size="200" :roll="roll" :pitch="pitch" v-show="false"/>
-      <Heading class="indicator-bg" :size="200" :heading="heading"/>
+      <Airspeed id="airspeed" class="indicator-bg" :size="200" :airspeed="airspeed" />
+      <Heading id="heading" class="indicator-bg" :size="200" :heading="heading"/>
+      <button id="btn-green" class="btn-listening-action green-gradient"></button>
+      <button id="btn-red" class="btn-listening-action red-gradient"></button>
       <Variometer class="indicator-bg" :size="200" :vario="vario" v-show="false" />
-      <Airspeed class="indicator-bg" :size="200" :airspeed="airspeed" v-show="false" />
+      <Attitude class="indicator-bg" :size="200" :roll="roll" :pitch="pitch" v-show="false"/>
       <Altimeter class="indicator-bg" :size="200" :altitude="altitude" :pressure="pressure" v-show="false" />
     </div>
   </div>
@@ -49,8 +51,72 @@ export default {
 </script>
 
 <style scoped>
-#app {
+body {
+  background-color: grey;
+  margin: 0; /* Ensure there is no margin around the body */
+  padding: 0; /* Ensure there is no padding around the body */
+  height: 100vh; /* Make the body take up the full viewport height */
+  width: 100vw; /* Make the body take up the full viewport width */
+  display: flex; /* To ensure it can contain flex children if needed */
+  justify-content: center; /* Optional: Center content horizontally */
+  align-items: center; /* Optional: Center content vertically */
+}
+
+#main-view {
+  position: relative;
   text-align: center;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 20px;
+  width: 1280px;
+  margin: 60px auto;
+}
+
+#airspeed {
+  position: absolute;
+  left: 300px;
+  top: 200px;
+}
+
+#heading {
+  position: absolute;
+  left: 540px;
+  top: 75px;
+}
+
+#btn-green {
+  position: absolute;
+  left: 415px;
+  top: 425px;
+}
+
+#btn-red {
+  position: absolute;
+  left: 325px;
+  top: 425px;
+}
+
+.btn-listening-action {
+  width: 60px;
+  height: 60px;
+  border: none;
+  cursor: pointer;
+  border-radius: 50%;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3), 0 6px 20px rgba(0, 0, 0, 0.2);
+  transition: transform 0.1s;
+}
+
+.btn-listening-action:active {
+  transform: translateY(4px);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3), 0 3px 10px rgba(0, 0, 0, 0.2);
+}
+
+.green-gradient {
+  background: linear-gradient(to right, #00c851, #007e33);
+}
+
+.red-gradient {
+  background: linear-gradient(to right, #ff4444, #cc0000);
 }
 </style>
