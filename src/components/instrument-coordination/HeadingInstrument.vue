43a6ca6a1cffa987035f59912287085d5185567e
Pandu Warjaya <panduwarjaya@gmail.com>
2024-07-30 07:25:39 +0700

    adjsut againa

--- src/components/instrument-coordination/HeadingInstrument.vue
+++ src/components/instrument-coordination/HeadingInstrument.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <Heading id="heading" class="indicator-bg" :size="200" :heading="headingValue" />
-    <div class="target" />
+    <canvas ref="targetCanvas" :width="width" :height="height" />
+    <div class="target" v-show="false" />
   </div>
 </template>
 
@@ -22,8 +23,18 @@ export default {
     return {
       headingValue: 0,
       signRandoms: ['+', '-'],
-      defaultIntervalHeader: 500, //in ms
+      defaultIntervalTarget: 500, //in ms
+      minimumIntervalTarget: 500, //in ms
+      maximumIntervalTarget: 2000, //in ms
       target: 0,
+      targetIncrement: null,
+      width: 180,
+      height: 180,
+      animationFrameId: null,
+      greenDotX: 0,
+      greenDotY: 0,
+      greenDotColor: 'green',
+      radius: 80,
     }
   },
   created() {
@@ -31,6 +42,8 @@ export default {
   },
   beforeUnmount() {
     window.removeEventListener('keyup', this.handleKeyPress);
+    clearInterval(this.intervalTarget);
+    cancelAnimationFrame(this.animationFrameId);
   },
   computed: {
     rotationStyle() {
@@ -42,7 +55,8 @@ export default {
   },
   mounted: function () {
     if (this.changeType !== 'inactive') {
-      this.executeHeadingMovement();
+      this.initTarget();
+      this.executeTargetMovement();
     }
   },
   methods: {
@@ -60,7 +74,7 @@ export default {
     getRandomInterval(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     },
-    executeHeadingMovement() {
+    executeTargetMovement() {
       const sign = this.getRandomOperator();
       if (sign === '+') {
         this.target += this.changeValue;
@@ -68,17 +82,78 @@ export default {
         this.target -= this.changeValue;
       }
 
-      let intervalHeader = this.defaultIntervalHeader; //in ms
+      let intervalTarget = this.defaultIntervalTarget; //in ms
       if (this.changeType === 'adjust_for_irregular_updates') {
-        intervalHeader = this.getRandomInterval(1000, 3000);
+        intervalTarget = this.getRandomInterval(this.minimumIntervalTarget, this/this.maximumIntervalTarget);
       }
-      setTimeout(this.executeHeadingMovement, intervalHeader);
+      setTimeout(this.executeTargetMovement, intervalTarget);
     },
     getRandomOperator() {
       const randomIndex = Math.floor(Math.random() * this.signRandoms.length);
       return this.signRandoms[randomIndex];
     },
+    initTarget() {
+      this.animate();
+      this.greenDotX = this.width / 2;
+      this.greenDotY = this.height / 2;
+    },
+    drawTarget() {
+      const canvas = this.$refs.targetCanvas;
+      const ctx = canvas.getContext('2d');
 
+      // Clear canvas
+      ctx.clearRect(0, 0, this.width, this.height);
+
+      // Center coordinates
+      const centerX = this.width / 2;
+      const centerY = this.height / 2;
+
+      // Draw main circle
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, this.radius, 0, 2 * Math.PI);
+      ctx.strokeStyle = 'blue';
+      ctx.stroke();
+
+      // Draw axes
+      ctx.beginPath();
+      ctx.moveTo(centerX, 0);
+      ctx.lineTo(centerX, this.height);
+      ctx.moveTo(0, centerY);
+      ctx.lineTo(this.width, centerY);
+      ctx.strokeStyle = 'grey';
+      ctx.stroke();
+
+      // Koordinat objek kuning
+      const yellowX = centerX + this.radius * Math.cos(this.target);
+      const yellowY = centerY + this.radius * Math.sin(this.target);
+
+      // Hitung titik-titik segitiga
+      const triangleSize = 20; // Ukuran segitiga
+      const angleOffset = Math.PI / 6; // Offset sudut untuk segitiga
+
+      const point1X = yellowX;
+      const point1Y = yellowY;
+
+      const point2X = yellowX + triangleSize * Math.cos(this.target + angleOffset);
+      const point2Y = yellowY + triangleSize * Math.sin(this.target + angleOffset);
+
+      const point3X = yellowX + triangleSize * Math.cos(this.target - angleOffset);
+      const point3Y = yellowY + triangleSize * Math.sin(this.target - angleOffset);
+
+      // Gambar segitiga
+      ctx.beginPath();
+      ctx.moveTo(point1X, point1Y); // Titik puncak segitiga
+      ctx.lineTo(point2X, point2Y); // Titik kiri bawah segitiga
+      ctx.lineTo(point3X, point3Y); // Titik kanan bawah segitiga
+      ctx.closePath(); // Menghubungkan titik terakhir dengan titik pertama
+
+      ctx.fillStyle = 'red'
+      ctx.fill();
+    },
+    animate() {
+      this.drawTarget();
+      this.animationFrameId = requestAnimationFrame(this.animate);
+    },
   }
 }
 </script>
@@ -109,5 +184,10 @@ export default {
   left: 640px;
   top: 105px;
   transform: translate(-50%, -50%);
+}
+
+canvas {
+  border: 0px solid black;
+  margin-top: 20px;
 }
 </style>
