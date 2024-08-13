db5b4d255a55d666782c70b09f2125c5ac6bd11f
Pandu Warjaya <panduwarjaya@gmail.com>
2024-08-13 22:41:14 +0700

    completed generate question

--- src/components/SymbolAddition.vue
+++ src/components/SymbolAddition.vue
@@ -9,13 +9,13 @@
         </div>
       </div>
       <div class="w-4/5 mx-auto mt-5">
-        <div class="grid grid-rows-2 grid-cols-17 gap-2">
-          <div v-for="(question, index) in questions" :key="index + '_question'" :class="index % 2 === 0 ? '' : 'font-bold'">{{ index % 2 === 0 ? question.symbol : question }}</div>
-          <div v-for="(_, index) in questions" :key="index + '_answer'">
-            <div v-if="index % 2 === 0">&nbsp;</div>
+        <div class="grid grid-rows-2 grid-cols-17 gap-2 mb-3 p-2" v-for="(questionRows, indexRow) in questions.splice((currentPage-1) * totalRow, totalRow)" :key="indexRow + '_grid'" :class="(indexRow+1) === currentTask ? 'border border-violet-500 rounded' : ''">
+          <div v-for="(question, indexColumn) in questionRows" :key="indexColumn + '_question'" :class="indexColumn % 2 === 0 ? '' : 'font-bold'">{{ indexColumn % 2 === 0 ? question.symbol : question }}</div>
+          <div v-for="(_, indexColumn) in questionRows" :key="indexColumn + '_answer'">
+            <div v-if="indexColumn % 2 === 0">&nbsp;</div>
             <div v-else>
               <input
-                :name="'answer-' + index"
+                :name="'answer-' + indexColumn"
                 type="radio"
                 class="accent-violet-500"
                 v-for="n in 3"
@@ -35,13 +35,14 @@ export default {
     return {
       testName: 'Symbol Addition',
       currentTask: 1,
+      currentPage: 1,
       numberOfTask: null,
       selectedSymbols: null,
       resetQueryBarPerRow: null,
       varianceSymbols: null,
       isConfigLoaded: false,
       radioValues: [],
-      totalRow: 10,
+      totalRow: 8,
       choicesLength: 8,
       queryBarLength: 16,
       queryBars: [],
@@ -115,17 +116,22 @@ export default {
     },
     generateQuestion() {
       for (let i = 0; i < this.numberOfTask; i++) {
+        // Initialize each row as an empty array
+        this.questions[i] = [];
         const symbolsLength = this.queryBars.length;
 
         for (let j = 0; j < this.totalQuestionPerRow; j++) {
-            // Add symbol
-            this.questions.push(this.queryBars[j % symbolsLength]);
-            // Add random number
-            this.questions.push(this.getRandomNumberQuestion());
+          // Add a random symbol
+          const randomSymbolIndex = Math.floor(Math.random() * symbolsLength);
+          this.questions[i].push(this.queryBars[randomSymbolIndex]);
+
+          // Add a random number
+          this.questions[i].push(this.getRandomNumberQuestion());
         }
 
-        // Add the final symbol
-        this.questions.push(this.queryBars[this.totalQuestionPerRow % symbolsLength]);
+        // Add the final random symbol
+        const finalRandomSymbolIndex = Math.floor(Math.random() * symbolsLength);
+        this.questions[i].push(this.queryBars[finalRandomSymbolIndex]);
       }
     },
     getRandomNumberQuestion() {
