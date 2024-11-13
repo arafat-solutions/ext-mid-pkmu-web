401413e82f66e9473428ccba486318e06d7265f3
Ervan A Pambudi <Ervan.Agus@astragraphia.co.id>
2024-11-13 21:15:58 +0700

    perbaikan

--- src/components/InstrumentMultitask.vue
+++ src/components/InstrumentMultitask.vue
@@ -69,8 +69,6 @@ export default {
       isPauseGaugesMeter: false,
       configs: [],
       minuteTest: null,
-
-
       testName: 'Multitasking With Instrument',
       isLoading: false,
       minuteTime: null,
