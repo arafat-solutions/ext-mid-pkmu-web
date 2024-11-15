635d5fa7140de28e610b4a22b2b7d38989df6e42
firllyw <fwahyudi17@gmail.com>
2024-11-15 13:29:16 +0700

    check

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'dist',
+  devServer: {
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: 'wss://pkmu-backoffice.dispsiau.org/ws'
+    },
+  },
 })
